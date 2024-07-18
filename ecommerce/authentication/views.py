22646a35d3c0f34ba95b5a2e6fd83a8ad0efe264
Uttam Kumar <uttamkumar050519@gmail.com>
2024-07-18 23:53:28 +0530

    category module added

--- ecommerce/authentication/views.py
+++ ecommerce/authentication/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def signUp(request):
+    pass
+
+def signIn(request):
+    pass
+
+def signOut(request):
+    pass
+
+def resetPassword(request):
+    pass
+
+def forgetPassword(request):
+    pass
+
